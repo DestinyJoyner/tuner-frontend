7b0c67db4a5617f2c6370ce2a0d34bcb8c94c854
DestinyJoyner <destinyjoyner@pursuit.org>
2023-01-24 22:27:14 -0500

    album and song show page css complete, add song to album tbd

--- src/Components/AlbumsShowPage.js
+++ src/Components/AlbumsShowPage.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useContextProvider } from "./Provider";
+import plaques from "../assets/album-plaques.png"
+import cd from "../assets/cd.png"
 import "./AlbumsShowPage.css"
     
 function AlbumsShowPage() {
@@ -21,7 +23,33 @@ function AlbumsShowPage() {
 
     return (
         <div className='albumShow center-page'>
-            <h1>Album Show Page</h1>
+            <div className="cover center-page background-image">
+            <img src={plaques} alt="album-plaques" />
+            <section className="album-stats">
+                <img src ={cd} alt="cd" />
+                <p>
+                    <span>Album: {thisAlbum.album_name}</span>
+                    <span>Artist: {thisAlbum.album_artist}</span>
+                    <span>Released: {thisAlbum.released}</span>
+                    <span>Copies Sold: {thisAlbum.units_sold}</span>
+                </p>
+            </section>
+            <section className="album-songs">
+                <h3>Track List</h3>
+                <hr />
+                {
+                    albumSongs.map(({name, time, id}) =>
+                        <li key={id}>
+                            <Link to={`/songs/${id}`}>
+                                <span>{name}</span>
+                            </Link>
+                            <span>{time}</span>
+                        </li>
+                        
+                    )
+                }
+            </section>
+            </div>
         </div>
     );
 }
